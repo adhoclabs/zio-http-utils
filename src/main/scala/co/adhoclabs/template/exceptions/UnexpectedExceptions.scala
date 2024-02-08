243d627655ef049e3ff902908ecee2bcf24e2c04
Bill Frasure <bill@adhoclabs.co>
2024-02-08 16:00:28 -0700

    Fix broken refs

--- src/main/scala/co/adhoclabs/template/exceptions/UnexpectedExceptions.scala
+++ src/main/scala/co/adhoclabs/template/exceptions/UnexpectedExceptions.scala
@@ -1,10 +1,6 @@
 package co.adhoclabs.template.exceptions
 
 import co.adhoclabs.model.ErrorResponse
-import co.adhoclabs.template.models.Album
 
 abstract class UnexpectedException(val errorResponse: ErrorResponse) extends Exception(errorResponse.error) {}
 
-case class AlbumNotCreatedException(album: Album) extends UnexpectedException(
-  ErrorResponse(s"Unknown error creating album entitled ${album.title}.")
-)
--- src/main/scala/co/adhoclabs/template/exceptions/ValidationExceptions.scala
+++ src/main/scala/co/adhoclabs/template/exceptions/ValidationExceptions.scala
@@ -1,13 +1,8 @@
 package co.adhoclabs.template.exceptions
 
 import co.adhoclabs.model.ErrorResponse
-import co.adhoclabs.template.models.CreateAlbumRequest
 
 abstract class ValidationException(val errorResponse: ErrorResponse) extends Exception(errorResponse.error) {}
-
-case class NoSongsInAlbumException(createAlbumRequest: CreateAlbumRequest) extends ValidationException(
-  ErrorResponse(s"Not creating album entitled ${createAlbumRequest.title} because it had no songs.")
-)
 
 case class SongAlreadyExistsException(duplicateKeyMessage: String) extends ValidationException(
   ErrorResponse(duplicateKeyMessage)
