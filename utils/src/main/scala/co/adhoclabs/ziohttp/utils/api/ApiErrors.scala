2793e5229172b3cb9057d4c458c4df0b5ecc9f55
Bill Frasure <bill@adhoclabs.co>
2024-02-22 14:16:51 -0700

    More logging

--- utils/src/main/scala/co/adhoclabs/ziohttp/utils/api/ApiErrors.scala
+++ utils/src/main/scala/co/adhoclabs/ziohttp/utils/api/ApiErrors.scala
@@ -3,6 +3,7 @@ package co.adhoclabs.ziohttp.utils.api
 import co.adhoclabs.ziohttp.Schemas._
 import co.adhoclabs.ziohttp._
 import co.adhoclabs.ziohttp.utils.exceptions.{UnexpectedException, ValidationException}
+import org.slf4j.{Logger, LoggerFactory}
 import zio._
 import zio.http._
 import zio.http.endpoint.{Endpoint, EndpointMiddleware}
@@ -10,6 +11,7 @@ import zio.http.endpoint.{Endpoint, EndpointMiddleware}
 import scala.concurrent.Future
 
 object ApiErrors {
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
   def attachStandardErrors[A, B, C, D <: EndpointMiddleware](
                                                               endpoint: Endpoint[A, B, ZNothing, C, D]
                                                             ): Endpoint[A, B, Either[BadRequestResponse, InternalErrorResponse], C, D] =
@@ -33,9 +35,10 @@ object ApiErrors {
     case validationException: ValidationException =>
       Left(BadRequestResponse(validationException.errorResponse))
     case unexpectedException: UnexpectedException =>
+      logger.error("UnexpectedExpception", unexpectedException)
       Right(InternalErrorResponse(unexpectedException.errorResponse))
     case exception: Throwable =>
-      println("Unexpected exception: " + exception.getMessage)
+      logger.error("Unexpected throwable: " + exception.getMessage, exception)
       Right(InternalErrorResponse(exception.getMessage))
   }
 
