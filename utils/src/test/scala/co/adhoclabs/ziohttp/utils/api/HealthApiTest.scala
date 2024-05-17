829a886376f083a5b30576d4f3d1d3feda7f481c
Bill Frasure <bill@adhoclabs.co>
2024-05-17 10:39:56 -0600

    Fix test error

--- utils/src/test/scala/co/adhoclabs/ziohttp/utils/api/HealthApiTest.scala
+++ utils/src/test/scala/co/adhoclabs/ziohttp/utils/api/HealthApiTest.scala
@@ -1,11 +1,9 @@
 package co.adhoclabs.ziohttp.utils.api
 
-import zio.http.{Request, Status}
-
-import scala.concurrent.Future
+import zio.http.{Request, Routes, Status}
 
 class HealthApiTest extends ApiTestBase {
-  val app = HealthRoutes.api.toHttpApp
+  val app = Routes(HealthRoutes.api)
 
   describe("GET /health/api") {
     it("should return a 200 response with an empty body") {
